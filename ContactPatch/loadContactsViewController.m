9dd8c9c1e982b184764138302b216922a5cedd4f
Vu Minami <admin@linhtm-pc.mobifone.vn>
2018-06-13 12:47:05 +0700

    ok

---
+++ ContactPatch/loadContactsViewController.m
@@ -0,0 +1,37 @@
+//
+//  loadContactsViewController.m
+//  ContactPatch
+//
+//  Created by admin on 6/13/18.
+//  Copyright © 2018 xmivu. All rights reserved.
+//
+
+#import "loadContactsViewController.h"
+
+@interface loadContactsViewController ()
+
+@end
+
+@implementation loadContactsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
