42406f14d76f984f109b15c10348c5b3cf30ccdf
P.KTDH-TC&TK O.Nam <namvx@vms.com.vn>
2018-06-13 09:36:54 +0700

    Initial Commit

---
+++ ContactPatch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ContactPatch
+//
+//  Created by admin on 6/13/18.
+//  Copyright © 2018 xmivu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
